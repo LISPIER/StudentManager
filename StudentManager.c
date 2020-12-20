a177a76e5ded772b115ee180ee2a8679e0460c31
永远的红十月 <lispier@163.com>
2020-12-20 22:27:41 +0800

    change()函数会在被调用两次后稳定退出，原因未知

--- StudentManager.c
+++ StudentManager.c
@@ -5,352 +5,265 @@
 #define BIG 100
 
 typedef struct{
-    int id;
-    char name[12];
+    char id[17];
+    char name[17];//精心设计的长度
     int chi;
     int math;
 } Student;
 
 Student StudentList[BIG];
-Student* CurrentOne=&StudentList[0];//这个指针很重要，她会永远指向下一个“等待被输入的槽位”，而每次输出只需要到她的前一位即可
-//由于每次删除都会将前后两部分合并，那么每次删除只需要把这个指针向前调一位就好了
-//不明白的话就去看del函数的代码！！！
+Student* NextOne=&StudentList[0];
 
-void menu();
-void apd();
-void prt();void prt_1(Student* p,int ShowTitle);
-void chg();void chg_1(Student* p);
-void del();
-void sch();
-void rnk();
-int lgn();
-void svf();
-void ldf();
-void ext();
-void eat();
-void stop();
+//一些结构
+int menu();
+
+//主要的功能函数
+void new();
+void view(); void ViewSingle(int pos,int ViewTitle);
+void change();
+void delete();
+void search();
+void rank();
+void read();
+void save();
+void login();
+void logout();
+
+//工具
 int score(Student* p);
-//void StuCpy();
+int find(char* id);
+
+//一些零碎的函数
+void eat();
 
 int main(){
-    while(1){
-        int lgn_return_value=lgn();
-        if(lgn_return_value==0){
-            printf("登陆成功");
-            stop();
-            break;
-        }
-        else if(lgn_return_value==1){
-            printf("用户名错误\n");
-        }
-        else if(lgn_return_value==2){
-            printf("密码错误\n");
-        }
-    }
+    //登录
+    login();
 
+    //菜单循环
     while(1){
+        int menu_return=menu();
         system("cls");
-        menu();
-        printf(">>>");
-        int choose;
-        scanf("%d",&choose);
-        eat();
-        system("cls");
-        switch(choose){
-            case 1:apd();break;
-            case 2:prt();break;
-            case 3:chg();break;
-            case 4:del();break;
-            case 5:sch();break;
-            case 6:rnk();break;
-            case 7:lgn();break;
-            case 8:svf();break;
-            case 9:ldf();break;
-            case 0:ext();break;
+        switch(menu_return){
+            case 1:new();break;
+            case 2:view();break;
+            case 3:change();break;
+            case 4:delete();break;
+            case 5:search();break;
+            case 6:rank();break;
+            case 7:login();break;
+            case 8:read();break;
+            case 9:save();break;
+            case 0:logout();break;
         }
-        stop();
-        if(choose==0){
+        system("pause");
+        if(menu_return==0){
             break;
         }
     }
     return 0;
 }
 
-void menu(){
-    printf("\t\t功能菜单\n");
-    printf("<1>添加\t\t\t\t<6>排序\n");
-    printf("<2>查看\t\t\t\t<7>登录\n");
-    printf("<3>修改\t\t\t\t<8>保存为文件\n");
-    printf("<4>删除\t\t\t\t<9>从文件加载\n");
-    printf("<5>搜索\t\t\t\t<0>退出\n");
+//显示功能菜单，返回用户所选择的值
+int menu(){
+    system("cls");
+    printf("\t功能菜单\n");
+    printf("<1>新建记录\t<6>排序显示\n");
+    printf("<2>显示全部\t<7>从文件读取\n");
+    printf("<3>修改记录\t<8>保存至文件\n");
+    printf("<4>删除记录\t<9>登录界面\n");
+    printf("<5>搜索记录\t<0>退出系统\n");
+    printf(">>>");
+    int choose;
+    scanf("%d",&choose);eat();
+    return choose;
 }
 
-//添加
-void apd(){
-    int id,chi,math;
-    char name[12];
-    printf("添加\n\n");
-    printf("请输入学号:");
-    scanf("%d",&id);
-    eat();
-    for(Student* p=&StudentList[0];p!=CurrentOne;p++){
-        if(id==(*p).id){
-            printf("学号重复，请重试\n");
-            return;
+void new(){
+    char id[BIG],name[BIG];
+    int chi,math;
+    printf("当前操作:向学生列表的末尾添加一条新的学生记录\n\n");
+    while(1){
+        printf("请输入学号(最多16位):");
+        scanf("%s",id);eat();
+        if(strlen(id)>16){
+            printf("输入过长\n");
+            continue;
         }
+        strcpy((*NextOne).id,id);
+        printf("%s\n",(*NextOne).id);
+        break;
     }
-    printf("请输入姓名:");
-    scanf("%s",name);
+    while(1){
+        printf("请输入姓名(最多16位，每个汉字占2位):");
+        scanf("%s",name);eat();
+        if(strlen(name)>16){
+            printf("输入过长\n");
+            continue;
+        }
+        strcpy((*NextOne).name,name);
+        printf("%s\n",(*NextOne).name);
+        break;
+    }
     printf("请输入语文成绩:");
-    scanf("%d",&chi);
-    printf("请输入数学成绩:");
-    scanf("%d",&math);
-    eat();
-    (*CurrentOne).id=id;
-    strcpy((*CurrentOne).name,name);
-    (*CurrentOne).chi=chi;
-    (*CurrentOne).math=math;
-    CurrentOne++;
+    scanf("%d",&chi);eat();
+    (*NextOne).chi=chi;
+    printf("请输入数学成绩");
+    scanf("%d",&math);eat();
+    (*NextOne).math=math;
+    NextOne++;//在创建了一条新纪录后，这个指针必须指向下一个空着的记录
 }
 
-//用于一次性输出所有同学的信息
-void prt(){
-    printf("学号\t姓名\t\t语文\t数学\t总分\n");
-    for(Student* p=&StudentList[0];p!=CurrentOne;p++){
-        prt_1(p,0);
-    }
-}
-
-//用于单独地输出一个同学的信息
-//出现了一个新问题，如果一个数据长度大于8，制表符会让整个输出看起来被偏移了
-//我的思路就是针对name这一项的输出进行更加精细的定制，以支持制表符
-//姓名到下一个数据之间的空间最长是16位，来自两个制表符
-//同时我们限定了姓名的最大长度为12位，不会超出，我们只需要在prt_1()上做亿点点调整就行了
-//也就是当姓名长度为8位及以上时，采用姓名后仅有一个制表符的输出语句
-//姓名长度小于8位时，采用姓名后有两个制表符的输出语句，和标题行保持一致
-void prt_1(Student* p,int ShowTitle){
-    if(ShowTitle==1){
-        printf("学号\t姓名\t\t语文\t数学\t总分\n");
-    }
-    if(strlen((*p).name)<8){
-        printf("%d\t%s\t\t%d\t%d\t%d\n",(*p).id,(*p).name,(*p).chi,(*p).math,score(p));
-    }
-    else{
-        printf("%d\t%s\t%d\t%d\t%d\n",(*p).id,(*p).name,(*p).chi,(*p).math,score(p));
-    }
-}
-
-//修改
-void chg(){
-    printf("修改\n\n");
-    printf("请输入学号");
-    int id;
-    scanf("%d",&id);
-    eat();
-    for(Student* p=&StudentList[0];p!=CurrentOne;p++){
-        if((*p).id==id){
-            prt_1(p,1);
-            chg_1(p);
+void view(){
+    printf("学号\t\t姓名\t\t语文\t\t数学\t\t总成绩\n");
+    for(int i=0;i<BIG;i++){
+        if(&StudentList[i]==NextOne){
             break;
         }
-        if(p==CurrentOne-1){
-            printf("没有找到\n");
-        }
+        ViewSingle(i,0);
     }
 }
 
-//修改一个学生的信息
-void chg_1(Student* p){
+//打印指定下标的学生信息
+//如果ViewTitle为1，则会在打印这个学生的信息时加上表头
+void ViewSingle(int pos,int ViewTitle){
+    if(ViewTitle==1){
+        printf("学号\t\t姓名\t\t语文\t\t数学\t\t总成绩\n");
+    }
+    Student* p=&StudentList[pos];
+    //打印学号
+    printf("%s",(*p).id);
+    if(strlen((*p).id)<8){
+        printf("\t\t");
+    }
+    else if(strlen((*p).id)!=16){
+        printf("\t");
+    }
+    //打印姓名
+    printf("%s",(*p).name);
+    if(strlen((*p).name)<8){
+        printf("\t\t");
+    }
+    else if(strlen((*p).name)!=16){
+        printf("\t");
+    }
+    //打印剩下的三个成绩
+    printf("%d\t\t%d\t\t%d\n",(*p).chi,(*p).math,score(p));
+}
+
+void change(){
+    printf("当前操作:通过学号查找学生信息，并修改\n\n");
+    char id[17];
+    char pos;
+    Student* p;
+    while(1){
+        printf("请输入一个有效的学号:");
+        scanf("%s",id);eat();
+        if(find(id)!=-1){
+            break;
+        }
+    }
+    pos=find(id);
+    ViewSingle(pos,1);
+    p=&StudentList[pos];
+    printf("请选择你想要变更的值\n");
+    printf("<1>姓名<2>语文<3>数学<其他>离开\n");
+    printf(">>>");
     int choose;
-    printf("请选择你想要修改的值\n<1>姓名\t<2>语文\t<3>数学<其他>离开\n");
-    scanf("%d",&choose);
-    eat();
+    scanf("%d",&choose);eat();
     if(choose==1){
-        char name[12];
-        printf("请输入新的姓名:");
-        scanf("%s",name);
-        eat();
-        strcpy((*p).name,name);
+        //修改姓名
+        printf("请输入修改后的姓名:");
+        scanf("%s",(*p).name);eat();
     }
     else if(choose==2){
-        int chi;
-        printf("请输入新的语文成绩:");
-        scanf("%d",&chi);
-        eat();
-        (*p).chi=chi;
+        //修改语文
+        printf("请输入修改后的语文成绩:");
+        scanf("%s",(*p).chi);eat();
     }
     else if(choose==3){
-        int math;
-        printf("请输入新的数学成绩:");
-        scanf("%d",&math);
-        eat();
-        (*p).math=math;
+        //修改数学
+        printf("请输入修改后的数学成绩:");
+        scanf("%s",(*p).math);eat();
     }
     else{
         return;
     }
+    printf("是否要继续修改?(Y/N))");
+    char YorN;
+    scanf("%c",&YorN);eat();
+    if(YorN=='Y'||YorN=='y'){
+        system("cls");
+        change();
+    }
 }
 
-//删除某一项后，该项后面的所有项目都会向前移动一位
-void del(){
-    printf("删除，在这个过程中被删去的数据，其之后的数据会补位。直到整个表连贯\n\n");
-    //新表的作用是将被删去的数据的前后的所有数据逐一放入
-    //然后再用新表覆盖旧表
-    Student NewList[BIG];
-    Student* np=&NewList[0];
-    printf("请输入学号:");
-    int id;
-    scanf("%d",&id);
-    eat();
-    //我是真的不喜欢把循环中使用的指针或数字定义成循环外可以访问的变量，所以在此处只能用pp把p的值从循环中带出来了
-    Student* pp=&StudentList[0];
-    for(Student* p=&StudentList[0];p!=CurrentOne;p++){
-        if((*p).id==id){
-            prt_1(p,1);
-            printf("你真的要删除以上数据吗(y/n)?");
-            char YorN;
-            while(1){
-                YorN=getchar();
-                eat();
-                if(YorN=='Y'||YorN=='y'){
-                    break;//跳出这个无限循环，然后走完下面的删除逻辑
-                }
-                else if(YorN=='N'||YorN=='n'){
-                    return;//直接结束这个函数，之前生成的新表全部木大
-                    //写到这里我是真的想重构整个系统了...
-                    //等我搞完了下周要交的作业再说...
-                }
-                else{
-                    printf("无效输入，请重试!");//Gold Experience Requiem!
-                }
-            }
-            pp=p;
+void delete(){}
+
+void search(){
+    printf("当前操作:通过学号查找学生信息\n\n");
+    printf("请输入一个有效的学号:");
+    char id[17];
+    scanf("%s",id);eat();
+    if(find(id)!=-1){
+        ViewSingle(find(id),1);
+    }
+}
+
+void rank(){}
+void read(){}
+void save(){}
+
+void login(){
+    char username[16]="沈霄阳";
+    char username_input[16];
+    char password[16]="26";
+    char password_input[16];
+
+    while(1){
+        printf("输入用户名:");
+        scanf("%s",username_input);eat();
+        if(strcmp(username,username_input)==0){
             break;
         }
-        (*np)=(*p);
-        np++;
-        if(p==CurrentOne-1){
-            printf("没有找到");
-            return;
-        }
+        printf("用户名错误!\n");
     }
-    for(Student* p=pp+1;p!=CurrentOne;p++){
-        (*np)=(*p);
-        np++;
-    }
-    np=&NewList[0];//把np重置
-    for(Student* p=&StudentList[0];p!=CurrentOne;p++){
-        (*p)=(*np);
-        np++;
-    }
-    CurrentOne--;//因为删去了一个数据，整个列表向前缩水了一位，所以这个指向第一个空位的指针也要向前拨动一位
-}
 
-//查询
-void sch(){
-    printf("搜索\n\n");
-    printf("请输入学号:");
-    int id;
-    scanf("%d",&id);
-    eat();
-    for(Student* p=&StudentList[0];p!=CurrentOne;p++){
-        if((*p).id==id){
-            prt_1(p,1);
+    while(1){
+        printf("输入密码:");
+        scanf("%s",password_input);eat();
+        if(strcmp(password,password_input)==0){
             break;
         }
-        if(p==CurrentOne-1){
-            printf("没有找到\n");
-        }
+        printf("密码错误!\n");
     }
 }
 
-//排序
-//原理很简单，对原来的表格做一次冒泡排序就行了
-void rnk(){
-    //创建一个备份表格用来暂时存储没有排序的原表格
-    Student StudentList_tmp[BIG];
-    Student* p_tmp=&StudentList_tmp[0];
-    for(Student* p=&StudentList[0];p!=CurrentOne;p++){
-        (*p_tmp)=(*p);
-        p_tmp++;
-    }
-    //以学生的总成绩为指标进行冒泡排序
-    for(Student* p=&StudentList[0];p!=CurrentOne;p++){
-        for(Student* pp=&StudentList[0];pp!=CurrentOne-1;pp++){
-            if(score(pp)<score(pp+1)){
-                Student tmp=(*pp);
-                (*pp)=(*(pp+1));
-                (*(pp+1))=tmp;
-            }
-        }
-    }
-    prt();
-    //将备份的表格写回源表格
-    p_tmp=&StudentList_tmp[0];
-    for(Student* p=&StudentList[0];p!=CurrentOne;p++){
-        (*p)=(*p_tmp);
-        p_tmp++;
-    }
-}
-
-//登录功能
-//用户名错误返回1
-//密码错误返回2
-//登陆成功返回0
-int lgn(){
-    char name[12]="sxy";
-    char name_input[12];
-    char password[20]="205501020326";
-    char password_input[20];
-    printf("请输入用户名:");
-    scanf("%s",name_input);
-    eat();
-    if(strcmp(name,name_input)!=0){
-        return 1;
-    }
-    printf("请输入密码:");
-    scanf("%s",password_input);
-    eat();
-    if(strcmp(password,password_input)!=0){
-        return 2;
-    }
-    return 0;
-}
-
-void svf(){}
-void ldf(){}
-
-//向DEEMO致敬
-void ext(){
-    printf("在离开前不要忘记说最后一次再见\n");
+void logout(){
+    printf("在离开前，别忘记说最后一次再见。\n");
     printf("Never left without saying Goodbye.\n");
 }
 
-void eat(){
-    //吃掉一个回车
-    getchar();
-}
-
-void stop(){
-    printf("\n<按任意键进行下一步>\n");
-    getchar();
-}
-
+//输入一个学生的指针
+//返回该学生的总成绩
 int score(Student* p){
     return (*p).chi+(*p).math;
 }
 
-/*
-//针对Student结构体编写的复制函数
-//其实这个函数根本没有必要，用(*NewStu)=(*Stu)也可以
-//但我像个憨憨一样写了这玩意那就接着拿来用吧...
-//至少这东西可以拿来改成某种可定制的复制函数，这是唯一比等号优秀的地方...
-//为了证明我不是习惯性绕路而是高瞻远瞩我又加了五行注释，真的没这个必要
-//别学我，你看我都写了六行废话了
-void StuCpy(Student* NewStu,Student* Stu){
-    (*NewStu).id=(*Stu).id;
-    strcpy((*NewStu).name,(*Stu).name);
-    (*NewStu).chi=(*Stu).chi;
-    (*NewStu).math=(*Stu).math;
+//给出学号，尝试通过学号查找一个对象
+//查找到则返回下标
+//没找到则返回-1
+int find(char* id){
+    int pos;
+    for(pos=0;pos<BIG;pos++){
+        if(strcmp(StudentList[pos].id,id)==0){
+            return pos;
+        }
+    }
+    printf("没有找到\n");
+    return -1;
 }
-*/
+
+void eat(){
+    getchar();
+}
